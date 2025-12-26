c928786ff6a336aab25aeb6c1940bb73bcbebcb6
jcaccavo <goredsox747@gmail.com>
2025-12-26 22:39:38 +0100

    Update 12c_3enzymesFilter.sh

--- 3RADseq/12c_3enzymesFilter.sh
+++ 3RADseq/12c_3enzymesFilter.sh
@@ -2,11 +2,11 @@
 inPath=".../12_Filter3rdEnzyme"
 outPath=".../Filtered3enzymes"
 
-for FASTQ in ${inPath}/*.fastq; do
+for FASTQ in ${inPath}/*.fastq.gz; do
         FILE=$(basename $FASTQ)
         echo $FILE
-        OUT=${FILE/.fastq/}
-        OUT1=${FILE/.R*.fastq/}
-        python2 .../Filter_reads.py -n -u -a -i ${inPath}/RestSite_Headers_${OUT1}.txt ${inPath}/${FILE} ${outPath}/${OUT}_3enzymes_filtered.fastq
+        OUT=${FILE/.fastq.gz/}
+        OUT1=${FILE/.R*.fastq.gz/}
+        python2 .../Filter_reads.py -n -u -a -i ${inPath}/RestSite_Headers_${OUT1}.txt ${inPath}/${FILE} ${outPath}/${OUT}_3enzymes_filtered.fastq.gz
 done
 
