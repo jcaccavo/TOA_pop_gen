350f3be9e5c1679c007e0a75f56a28e6ac38eb1a
jcaccavo <goredsox747@gmail.com>
2025-12-26 17:48:15 +0100

    Update getInsertSizefromSAM.sh

--- 3RADseq/getInsertSizefromSAM.sh
+++ 3RADseq/getInsertSizefromSAM.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-inPath="/srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/07_mapping2021"
+inPath=".../07_mapping"
 
 for SAM in $inPath/*.sam;do
         echo $SAM
         OUT=${SAM/.sam/}
-        python2 /srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/x_scripts/getInsertSizefromSAM.py $SAM $OUT.FragSizes
+        python2 .../getInsertSizefromSAM.py $SAM $OUT.FragSizes
         cut -f1 $OUT.FragSizes | sort | uniq -c > $OUT.Lengths
-        Rscript /srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/x_scripts/plotFragSizes_args_fastq.R $OUT.Lengths $OUT.svg
+        Rscript .../plotFragSizes_args_fastq.R $OUT.Lengths $OUT.svg
 done
