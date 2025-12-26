e67aec21d65bbece0a55c0bd4f3f0b93becf9ee1
jcaccavo <goredsox747@gmail.com>
2025-12-26 17:18:20 +0100

    Update 05_PCRduplicatesFilter.sh

--- 3RADseq/05_PCRduplicatesFilter.sh
+++ 3RADseq/05_PCRduplicatesFilter.sh
@@ -10,5 +10,5 @@ for r1 in *_1.fastq.gz;do
         echo $r1
         r2=$(echo $r1 | sed 's/_1.fastq.gz/_2.fastq.gz/')
         echo $r2
-        python2.7 $scriptPath/filterPCRdups_CM.py -r1 $r1 -r2 $r2 -o $outPath
+        python2.7 $scriptPath/filterPCRdups.py -r1 $r1 -r2 $r2 -o $outPath
 done
