4872ab7aca366c3b5e51013a8669921d428fca03
jcaccavo <goredsox747@gmail.com>
2025-12-26 22:38:26 +0100

    Update 12c_3enzymesFilter.sh

--- 3RADseq/12c_3enzymesFilter.sh
+++ 3RADseq/12c_3enzymesFilter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-inPath="/srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/12_Filter3rdEnzyme"
-outPath="/srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/12_Filter3rdEnzyme/Filtered3enzymes"
+inPath=".../12_Filter3rdEnzyme"
+outPath=".../Filtered3enzymes"
 
 for FASTQ in ${inPath}/*.fastq; do
         FILE=$(basename $FASTQ)
         echo $FILE
         OUT=${FILE/.fastq/}
         OUT1=${FILE/.R*.fastq/}
-        python2 /srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/x_scripts/Filter_reads.py -n -u -a -i ${inPath}/RestSite_Headers_${OUT1}.txt ${inPath}/${FILE} ${outPath}/${OUT}_3enzymes_filtered.fastq
+        python2 .../Filter_reads.py -n -u -a -i ${inPath}/RestSite_Headers_${OUT1}.txt ${inPath}/${FILE} ${outPath}/${OUT}_3enzymes_filtered.fastq
 done
 
