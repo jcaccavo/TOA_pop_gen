9c5158e9e1c354cdff6cfddff498a639c0d6736f
jcaccavo <goredsox747@gmail.com>
2025-12-26 22:36:03 +0100

    Update 12a_EditHeaders.sh

--- 3RADseq/12a_EditHeaders.sh
+++ 3RADseq/12a_EditHeaders.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-inPath="/srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/11_CheckRestSites"
-outPath="/srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/12_Filter3rdEnzyme"
+inPath=".../11_CheckRestSites"
+outPath=".../12_Filter3rdEnzyme"
 
-for r1 in ${inPath}/*_R1_checkrestsites.fastq;do
-        r2=$(echo $r1 | sed 's/_R1_checkrestsites.fastq/_R2_checkrestsites.fastq/')
+for r1 in ${inPath}/*_R1_checkrestsites.fastq.gz;do
+        r2=$(echo $r1 | sed 's/_R1_checkrestsites.fastq.gz/_R2_checkrestsites.fastq.gz/')
         FILE1=$(basename $r1)
         FILE2=$(basename $r2)
 	echo $r1
 	echo $FILE1
 	echo $FILE2
-        OUT1=${FILE1/_R1_checkrestsites.fastq/.R1.fastq}
-        OUT2=${FILE2/_R2_checkrestsites.fastq/.R2.fastq}
+        OUT1=${FILE1/_R1_checkrestsites.fastq.gz/.R1.fastq.gz}
+        OUT2=${FILE2/_R2_checkrestsites.fastq.gz/.R2.fastq.gz}
         echo $OUT1
         echo $OUT2
         cat $r1 | sed '/^@A/ s/\s.*$//' > ${outPath}/${OUT1}
