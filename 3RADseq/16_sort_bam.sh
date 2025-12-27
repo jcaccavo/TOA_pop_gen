c80a194deff8675a796234ca72839b57cf9d985a
jcaccavo <goredsox747@gmail.com>
2025-12-27 16:19:23 +0100

    Update 16_sort_bam.sh

--- 3RADseq/16_sort_bam.sh
+++ 3RADseq/16_sort_bam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq/01_RADseq/13_final_mapping
+cd .../13_final_mapping
 
 for bam in *.bam;do
         file=$(basename $bam | sed 's/.bam//') #basename give you the name without the path
