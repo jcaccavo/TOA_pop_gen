b3526d47db4d307d96ca514894bc5fb6008e4778
jcaccavo <goredsox747@gmail.com>
2025-12-27 16:16:35 +0100

    Update 15_convert_sam_to_bam.sh

--- 3RADseq/15_convert_sam_to_bam.sh
+++ 3RADseq/15_convert_sam_to_bam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /srv/public/users/jcaccavo/11_CCGA_full_seq/02_NovaSeq//01_RADseq/13_final_mapping
+cd .../13_final_mapping
 
 for sam in *.sam;do
         bam=$(echo $sam | sed 's/.sam/.bam/')
