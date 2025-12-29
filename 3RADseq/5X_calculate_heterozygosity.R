a374f92b65fb00cf6d078bcb163d9446eb7cdeaa
jcaccavo <goredsox747@gmail.com>
2025-12-29 17:10:34 +0100

    Add files via upload

---
+++ 3RADseq/5X_calculate_heterozygosity.R
@@ -0,0 +1,130 @@
+# Calculate heterozygosity from realSFS output .ml file
+
+setwd("/Users/JMAC/Library/CloudStorage/Dropbox/Research/Humboldt/CCGA_full_sequencing/ANGSD/heterozygosity/realSFS_folded_estimates_ml/5X")
+
+# 5X samples
+
+sample_1_425_5X<-scan("DoSaf_1-425_5X_est.ml")
+sample_1_425_5X_heterozygosity<-data.frame(sample_1_425_5X[2]/sum(sample_1_425_5X))
+sample_1_425_5X_heterozygosity
+# 0.002183355
+
+sample_168_5X<-scan("DoSaf_168_5X_est.ml")
+sample_168_5X_heterozygosity<-data.frame(sample_168_5X[2]/sum(sample_168_5X))
+sample_168_5X_heterozygosity
+# 0.003887137
+
+sample_312_5X<-scan("DoSaf_312_5X_est.ml")
+sample_312_5X_heterozygosity<-data.frame(sample_312_5X[2]/sum(sample_312_5X))
+sample_312_5X_heterozygosity
+# 0.003154198
+
+sample_313_5X<-scan("DoSaf_313_5X_est.ml")
+sample_313_5X_heterozygosity<-data.frame(sample_313_5X[2]/sum(sample_313_5X))
+sample_313_5X_heterozygosity
+# 0.003079298
+
+sample_314_5X<-scan("DoSaf_314_5X_est.ml")
+sample_314_5X_heterozygosity<-data.frame(sample_314_5X[2]/sum(sample_314_5X))
+sample_314_5X_heterozygosity
+# 0.005192491
+
+sample_315_5X<-scan("DoSaf_315_5X_est.ml")
+sample_315_5X_heterozygosity<-data.frame(sample_315_5X[2]/sum(sample_315_5X))
+sample_315_5X_heterozygosity
+# 0.003778383
+
+sample_33_429_5X<-scan("DoSaf_33-429_5X_est.ml")
+sample_33_429_5X_heterozygosity<-data.frame(sample_33_429_5X[2]/sum(sample_33_429_5X))
+sample_33_429_5X_heterozygosity
+# 0.002120382
+
+sample_34_430_5X<-scan("DoSaf_34-430_5X_est.ml")
+sample_34_430_5X_heterozygosity<-data.frame(sample_34_430_5X[2]/sum(sample_34_430_5X))
+sample_34_430_5X_heterozygosity
+# 0.002068954
+
+sample_35_431_5X<-scan("DoSaf_35-431_5X_est.ml")
+sample_35_431_5X_heterozygosity<-data.frame(sample_35_431_5X[2]/sum(sample_35_431_5X))
+sample_35_431_5X_heterozygosity
+# 0.002060941
+
+sample_412_5X<-scan("DoSaf_412_5X_est.ml")
+sample_412_5X_heterozygosity<-data.frame(sample_412_5X[2]/sum(sample_412_5X))
+sample_412_5X_heterozygosity
+# 0.00263602
+
+sample_419_5X<-scan("DoSaf_419_5X_est.ml")
+sample_419_5X_heterozygosity<-data.frame(sample_419_5X[2]/sum(sample_419_5X))
+sample_419_5X_heterozygosity
+# 0.003149227
+
+sample_42_438_5X<-scan("DoSaf_42-438_5X_est.ml")
+sample_42_438_5X_heterozygosity<-data.frame(sample_42_438_5X[2]/sum(sample_42_438_5X))
+sample_42_438_5X_heterozygosity
+# 0.002098405
+
+sample_433_5X<-scan("DoSaf_433_5X_est.ml")
+sample_433_5X_heterozygosity<-data.frame(sample_433_5X[2]/sum(sample_433_5X))
+sample_433_5X_heterozygosity
+# 0.003967605
+
+sample_4_456_5X<-scan("DoSaf_4-456_5X_est.ml")
+sample_4_456_5X_heterozygosity<-data.frame(sample_4_456_5X[2]/sum(sample_4_456_5X))
+sample_4_456_5X_heterozygosity
+# 0.002057574
+
+sample_50_446_5X<-scan("DoSaf_50-446_5X_est.ml")
+sample_50_446_5X_heterozygosity<-data.frame(sample_50_446_5X[2]/sum(sample_50_446_5X))
+sample_50_446_5X_heterozygosity
+# 0.00211851
+
+sample_527_5X<-scan("DoSaf_527_5X_est.ml")
+sample_527_5X_heterozygosity<-data.frame(sample_527_5X[2]/sum(sample_527_5X))
+sample_527_5X_heterozygosity
+# 0.003536336
+
+sample_9432_5X<-scan("DoSaf_9432_5X_est.ml")
+sample_9432_5X_heterozygosity<-data.frame(sample_9432_5X[2]/sum(sample_9432_5X))
+sample_9432_5X_heterozygosity
+# 0.003723416
+
+sample_9479_5X<-scan("DoSaf_9479_5X_est.ml")
+sample_9479_5X_heterozygosity<-data.frame(sample_9479_5X[2]/sum(sample_9479_5X))
+sample_9479_5X_heterozygosity
+# 0.00432774
+
+sample_9506_5X<-scan("DoSaf_9506_5X_est.ml")
+sample_9506_5X_heterozygosity<-data.frame(sample_9506_5X[2]/sum(sample_9506_5X))
+sample_9506_5X_heterozygosity
+# 0.003256673
+
+sample_9512_5X<-scan("DoSaf_9512_5X_est.ml")
+sample_9512_5X_heterozygosity<-data.frame(sample_9512_5X[2]/sum(sample_9512_5X))
+sample_9512_5X_heterozygosity
+# 0.002753515
+
+sample_9522_5X<-scan("DoSaf_9522_5X_est.ml")
+sample_9522_5X_heterozygosity<-data.frame(sample_9522_5X[2]/sum(sample_9522_5X))
+sample_9522_5X_heterozygosity
+# 0.006743361
+
+sample_9525_5X<-scan("DoSaf_9525_5X_est.ml")
+sample_9525_5X_heterozygosity<-data.frame(sample_9525_5X[2]/sum(sample_9525_5X))
+sample_9525_5X_heterozygosity
+# 0.004954809
+
+sample_9540_5X<-scan("DoSaf_9540_5X_est.ml")
+sample_9540_5X_heterozygosity<-data.frame(sample_9540_5X[2]/sum(sample_9540_5X))
+sample_9540_5X_heterozygosity
+# 0.002701547
+
+sample_9544_5X<-scan("DoSaf_9544_5X_est.ml")
+sample_9544_5X_heterozygosity<-data.frame(sample_9544_5X[2]/sum(sample_9544_5X))
+sample_9544_5X_heterozygosity
+# 0.002987411
+
+sample_CSB_5X<-scan("DoSaf_CSB_5X_est.ml")
+sample_CSB_5X_heterozygosity<-data.frame(sample_CSB_5X[2]/sum(sample_CSB_5X))
+sample_CSB_5X_heterozygosity
+# 0.002280588
